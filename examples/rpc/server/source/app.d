c2b49d328ea1f18c82ea245dcf5c836b16cf5fa6
zoujiaqing <zoujiaqing@gmail.com>
2016-04-06 23:46:19 +0800

    add rpc example

---
+++ examples/rpc/server/source/app.d
@@ -0,0 +1,50 @@
+
+
+import core.thread;
+import std.stdio;
+import collie.channel;
+import collie.handler.base;
+import collie.booststrap.server;
+import std.parallelism;
+import core.runtime;
+import std.conv;
+import core.memory;
+import std.container.array;
+import collie.codec.ptpack.ptpack;
+
+extern(C) __gshared string[] rt_options = [ "gcopt=profile:0 incPoolSize:4" ];
+
+final class EchoHandle : Handler
+{
+	this (PiPeline pip) {super(pip);}
+
+	override void inEvent(InEvent event){
+            writeln("have new event :", event.type);
+		if(event.type == DePackEvent.type) {
+			scope auto ev = cast(DePackEvent) event;
+			writeln("have event , data = " , ev.mdata);
+			writeln("have event , type = " , ev.mtype);
+			writeln("have event , lenght = " , ev.mlength);
+			mixin(encodePack("ev.mdata.dup","ev.mtype","this.pipeline","this"));
+        } else {
+			event.up();
+		}
+
+	}
+}
+
+void createPipeline(PiPeline pip){
+     pip.pushHandle(new PtPack!(true)(pip));
+	pip.pushHandle(new EchoHandle(pip));
+}
+
+void main()
+{
+	writeln("current cpus = ",totalCPUs);
+	writeln("start echo server at port : 9009");
+	ServerBoostStarp server = new ServerBoostStarp;
+	server.setPipelineFactory(toDelegate(&createPipeline))
+		.setThreadSize(2)
+		.bind(Address("0.0.0.0",9009)).run();
+}
+
