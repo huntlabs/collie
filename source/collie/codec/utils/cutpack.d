fd0507d81a8c78958ad985ec6401918c44b71fb4
Azbuka_slovensko <azbuka.slovensko@gmail.com>
2016-04-26 20:48:30 +0200

    Fixed Codec/utils/cutpack

--- source/collie/codec/utils/cutpack.d
+++ source/collie/codec/utils/cutpack.d
@@ -9,47 +9,44 @@ import collie.handler.base;
 import collie.channel.pipeline;
 import collie.channel.define;
 
-string enCutPack(string data,string pipeline,string handler){
-	string str =  "{ scope auto mixins_event = new OutCutPackEvent(" ~ pipeline ~ ", " ~ handler ~ "); 
+string enCutPack(string data, string pipeline, string handler) {
+	string str = "{ scope auto mixins_event = new OutCutPackEvent(" ~ pipeline ~ ", " ~ handler ~ "); 
     mixins_event.data = " ~ data ~ ";
 	mixins_event.down(); }";
 	return str;
 }
 
-string enCutPack(string data,string event){
-	string str =  "{ scope auto mixins_event = new OutCutPackEvent(" ~ event ~ ");
+string enCutPack(string data, string event) {
+	string str = "{ scope auto mixins_event = new OutCutPackEvent(" ~ event ~ ");
     mixins_event.data = " ~ data ~ ";
 	mixins_event.down(); }";
 	return str;
 }
 
 
-class CutPack(bool littleEndian) : Handler 
-{
-	this(PiPeline pip,uint maxLen = uint.max)
-	{
+class CutPack(bool littleEndian) : Handler {
+	this(PiPeline pip,uint maxLen = uint.max) {
 		super(pip);
 		_max = maxLen;
 	}
 
-	~this(){
+	~this() {
 		_packData = null;
 	}
 
-	override void inEvent(InEvent event)
-	{
+	override void inEvent(InEvent event) {
 		trace("CutPack inEvent ", event.type);
 		if(event.type == INEVENT_TCP_READ) {
-			scope auto ev = cast(INEventTCPRead)event;
-			if(ev.data.length == 0) return;
+			scope auto ev = cast(INEventTCPRead) event;
+			if(ev.data.length == 0)
+				return;
 			readPack(ev.data,event);
 		} else {
 			event.up();
 		}
 	}
 	
-	override void outEvent(OutEvent event)
-	{
+	override void outEvent(OutEvent event) {
 		trace("CutPack outEvent  ", event.type);
 		if(event.type == OutCutPackEvent.type) {
 			scope auto ev = cast(OutCutPackEvent)event;
@@ -62,13 +59,13 @@ class CutPack(bool littleEndian) : Handler
 	}
 
 protected:
-	void readPack(ubyte[] data,InEvent event)
-	{
-		if(data.length == 0) return;
+	void readPack(ubyte[] data,InEvent event) {
+		if(data.length == 0)
+			return;
 		if(_size == 0) {
 			uint rang = readPackSize(data);
 			if(rang > 0) {
-				if(rang < cast(uint)data.length){ 
+				if(rang < cast(uint)data.length) { 
 					data = data[rang..$];
 					if(_size == 0) {
 						_pSize = 0;
@@ -78,47 +75,49 @@ protected:
 				} else { 
 					return;
 				}
-			} else return;
+			} else {
+				return;
+			}
 		} 
-		ubyte * tptr  = _packData.ptr + _readSize;
-		uint size = cast(uint)data.length;
+		ubyte* tptr = _packData.ptr + _readSize;
+		uint size = cast(uint) data.length;
 		uint tsize = _size - _readSize;
 		if(size >= tsize) {
 			memcpy(tptr, data.ptr, tsize);
 			_readSize = 0;
 			_pSize = 0;
 			_size = 0;
-			scope InCutPackEvent ev = new InCutPackEvent(event,_packData);
+			scope InCutPackEvent ev = new InCutPackEvent(event, _packData);
 			ev.up();
-			if(size == tsize) return;
+			if(size == tsize)
+				return;
 			size = 0;
 			data = data[tsize..$];
-			readPack(data,event);
+			readPack(data, event);
 		} else {
-			memcpy(tptr,data.ptr,size);
+			memcpy(tptr, data.ptr,size);
 			_readSize += size;
 		}
 	}
 
-
-	uint readPackSize(ref ubyte[] data)
-	{
-		if(_pSize == 4 || _size > 0) return 0;
-		uint size = cast(uint)data.length;
+	uint readPackSize(ref ubyte[] data) {
+		if(_pSize == 4 || _size > 0)
+			return 0;
+		uint size = cast(uint) data.length;
 		uint i;
-		for(i = 0; _pSize < 4 && i < size; ++i, ++ _pSize){
+		for(i = 0; _pSize < 4 && i < size; ++i, ++ _pSize) {
 			_packSize[_pSize] = data[i];
 		}
 		if(_pSize == 4) {
 			static if(littleEndian) {
 				_size = littleEndianToNative!uint(_packSize);//littleEndianToNative
-			} else  {
+			} else {
 				_size = bigEndianToNative!uint(_packSize);//
 			}
 			_readSize = 0;
-			if(_size == 0){
+			if(_size == 0)
 				return i;
-			}
+
 			if(_size > _max) {
 				mixin(closeChannel("this.pipeline","this"));
 				return 0;
@@ -130,8 +129,7 @@ protected:
 		}
 	}
 
-	ubyte[] writePack(ubyte[] edata)
-	{
+	ubyte[] writePack(ubyte[] edata) {
 		uint size = cast(uint)edata.length;
 		ubyte[] data = new ubyte[size + 4];
 		static if(littleEndian) {
@@ -155,8 +153,7 @@ private:
 };
 
 
-class InCutPackEvent : InEvent
-{
+class InCutPackEvent : InEvent {
 	shared static this() {
 		if(type == 0) {
 			type = getEventType();
@@ -166,13 +163,16 @@ class InCutPackEvent : InEvent
 	shared static const uint type;
 
 	ubyte[] data;
-	this(const InEvent ev){super(ev,type);}
+
 	@disable this();
-	this(const InEvent ev,ubyte[] data){super(ev,type);this.data = data;}
+	this(const InEvent ev) { super(ev,type); }
+	this(const InEvent ev, ubyte[] data) {
+		super(ev,type);
+		this.data = data;
+	}
 };
 
-class OutCutPackEvent : OutEvent
-{
+class OutCutPackEvent : OutEvent {
 	shared static this() {
 		if(type == 0) {
 			type = getEventType();
@@ -180,15 +180,18 @@ class OutCutPackEvent : OutEvent
 		}
 	}
 	shared static const uint type;
-	
+
 	ubyte[] data;
-	
-	this(const OutEvent ev) {super(ev,type);}
-	this(const PiPeline pip){super(pip,type);}
+
 	@disable this();
-	this(const OutEvent ev,ubyte[] data){super(ev,type);this.data = data;}
-	this(const PiPeline pip,OutHandle hand){
-		super(pip,type,hand);
+	this(const OutEvent ev) { super(ev,type); }
+	this(const PiPeline pip) { super(pip,type); }
+	this(const OutEvent ev, ubyte[] data) {
+		super(ev,type);
+		this.data = data;
+	}
+	this(const PiPeline pip, OutHandle hand) {
+		super(pip, type, hand);
 	}
 };
 
