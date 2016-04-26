7003ea041b9fa778d533d39a8755975e00a75650
Azbuka_slovensko <azbuka.slovensko@gmail.com>
2016-04-26 20:43:19 +0200

    Fixed Handler/base

--- source/collie/handler/base.d
+++ source/collie/handler/base.d
@@ -2,63 +2,58 @@ module collie.handler.base;
 
 import collie.channel.pipeline;
 
-enum HandleType
-{
+enum HandleType {
 	outHandle,
 	inHandle,
 	allHandle
-};
+}
 
-interface BeseHandle
-{
+interface BeseHandle {
 	HandleType handleType();
-};
+}
 
-interface OutHandle
-{
+interface OutHandle {
 	void outEvent(OutEvent event);
-};
+}
 
-interface InHandle
-{
+interface InHandle {
 	void inEvent(InEvent event);
-};
+}
 
-abstract class Handler : OutHandle,InHandle,BeseHandle
-{
-	this (PiPeline pip) {_pipeline = pip;}
-	final HandleType handleType(){return HandleType.allHandle;}
+abstract class Handler : OutHandle, InHandle, BeseHandle {
+	this(PiPeline pip) { _pipeline = pip; }
+	final HandleType handleType() { return HandleType.allHandle; }
 
-	override void inEvent(InEvent event){
+	override void inEvent(InEvent event) {
 		event.up();
 	}
-	
-	override void outEvent(OutEvent event){
+
+	override void outEvent(OutEvent event) {
 		event.down();
 	}
-	final @property const(PiPeline) pipeline() const {return _pipeline;}
+
+	final @property const(PiPeline) pipeline() const { return _pipeline; }
 private:
 	PiPeline _pipeline;
 };
 
-abstract class InHander : InHandle , BeseHandle
-{
-	final HandleType handleType(){return HandleType.inHandle;}
-	override void inEvent(InEvent event){
+abstract class InHander : InHandle , BeseHandle {
+	final HandleType handleType() { return HandleType.inHandle; }
+
+	override void inEvent(InEvent event) {
 		event.up();
 	}
 };
 
-abstract class OutHander : OutHandle,BeseHandle
-{
-	this (PiPeline pip) {_pipeline = pip;}
-	final HandleType handleType(){return HandleType.outHandle;}
+abstract class OutHander : OutHandle,BeseHandle {
+	this(PiPeline pip) { _pipeline = pip; }
+	final HandleType handleType() { return HandleType.outHandle; }
 
-	override void outEvent(OutEvent event){
+	override void outEvent(OutEvent event) {
 		event.down();
 	}
 
-	final @property const(PiPeline) pipeline() const {return _pipeline;}
+	final @property const(PiPeline) pipeline() const { return _pipeline; }
 private:
 	PiPeline _pipeline;
 };
