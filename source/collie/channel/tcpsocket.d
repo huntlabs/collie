83707587a6b0136b6a3931e9af8ee4700357da59
Azbuka_slovensko <azbuka.slovensko@gmail.com>
2016-04-26 18:21:39 +0200

    Fixed Channel/tcpsocket

--- source/collie/channel/tcpsocket.d
+++ source/collie/channel/tcpsocket.d
@@ -15,19 +15,17 @@ import collie.channel.utils.buffer;
  @date  2016.1
  */
 
-final class TCPSocket : Channel //添加客户端的链接之类的逻辑
-{
-public :
+final class TCPSocket : Channel { //添加客户端的链接之类的逻辑
+public:
 	/** 构造函数
 	 @param : loop = 所属的事件循环。
 	 */
-	this (EventLoop loop)
-	{
+	this(EventLoop loop) {
 		this(loop,-1);
 	}
+
 	/** 析构函数 */
-	~this ()
-	{
+	~this() {
 		onClose();
 		colliedAllocator.deallocate(_recvBuffer);
 	}
@@ -43,7 +41,6 @@ public :
 	}
 	/** 混入Socket 选项模板 */
 	mixin SocketOption!();
-
 	mixin TCPSocketMixin!(TCPListener);
 
 package:
@@ -51,8 +48,7 @@ package:
 	 @param : loop = 所属的事件循环。
 	 @param : socket = 此socket管理的FD。
 	 */
-	this (EventLoop loop, int socket)
-	{
+	this(EventLoop loop, int socket) {
 		super(loop);
 		type = CHANNEL_TYPE.TCP_Socket;
 		fd = socket;
@@ -64,8 +60,8 @@ package:
 		_sendQueue = SqQueue!(WriteBuffer,true)(SEND_QUEUE_SIZE);
 	}
 
-	void reset(EventLoop loop,int tfd) {
-		fd  = tfd;
+	void reset(EventLoop loop, int tfd) {
+		fd = tfd;
 		eventLoop = loop;
 		_status = SOCKET_STATUS.CONNECTED;
 	}
@@ -74,19 +70,17 @@ protected:
 	/** 有可读事件时调用的函数。
 	 @note : 用原子实现的同步，即使多个线程同时跑事件循环，也保证同时只有一个线程在执行次函数。
 	 */
-	final override void onRead () 
-	{
+	final override void onRead() {
 		trace("  onread");
 		ssize_t length = 0;
-		while (!isInValid())
-		{
+		while (!isInValid()) {
 			length = .read(fd, _recvBuffer.ptr, _recvBuffer.length);
 			trace("tcp on read: ",length);
-			if (length > 0) {
+			if(length > 0) {
 				trace("tcp on read data = : ", _recvBuffer[0..length]);
 				_recvHandler(_recvBuffer[0..length]);
 			} else {
-				if (errno == EWOULDBLOCK || errno == EAGAIN ){ // erro 4 :系统中断组织了
+				if(errno == EWOULDBLOCK || errno == EAGAIN ) { // erro 4 :系统中断组织了
 					break;
 				} else if(errno == 4) {
 					continue;
@@ -100,9 +94,8 @@ protected:
 	}
 
 	/** 从事件循环中移除socket并关闭 */
-	final override void onClose ()
-	{
-		if (isInValid()){
+	final override void onClose() {
+		if(isInValid()) {
 			return;
 		}
 		if(!_start) {
@@ -113,10 +106,10 @@ protected:
 		_start = false;
 		eventLoop.delEvent(this);
 		fd = -1;
-		if(!_sendQueue.empty){
+		if(!_sendQueue.empty) {
 			scope ubyte[][] buffer = new ubyte[][_sendQueue.length];
 			int i = 0;
-			while(!_sendQueue.empty){
+			while(!_sendQueue.empty) {
 				buffer[i] = _sendQueue.deQueue().allData;
 				++i;
 			}
@@ -129,27 +122,26 @@ protected:
 	}
 
 	/** 有可写事件时调用的函数。*/
-	final override void onWrite ()
-	{
+	final override void onWrite() {
 		trace("  onWrite");
 		if(status == SOCKET_STATUS.CONNECTING) { 
 			status(SOCKET_STATUS.CONNECTED);
 		}
-		if(_sendQueue.empty) return;
+		if(_sendQueue.empty)
+			return;
 		doWrite();
 	}
 protected:
 	/** 执行socket写入。
 	 */
-	final void doWrite()
-	{
+	final void doWrite() {
 		ssize_t length = 0;
 		WriteBuffer buffer;
 		while(!isInValid() && !_sendQueue.empty) {
 			buffer = _sendQueue.front();
 			length = .write(fd, buffer.data.ptr, buffer.dataSize);
-			if (length >  0) {
-				if (length < buffer.dataSize) {
+			if(length >  0) {
+				if(length < buffer.dataSize) {
 					buffer._start += length;
 					break;
 				} else {
@@ -157,59 +149,54 @@ protected:
 					_sendHandler(buffer.allData,cast(uint)buffer.allData.length);
 					continue;
 				}
-			} else  {
-				if (errno == EWOULDBLOCK || errno == EAGAIN)
-				{
+			} else {
+				if(errno == EWOULDBLOCK || errno == EAGAIN) {
 					break;
-				} else if (errno == 4){
+				} else if(errno == 4) {
 					continue;
-				}else{
+				} else {
 					onClose();
 				}
 			}
-
 		} 
 	}
-
 }
 
 
 mixin template TCPSocketMixin(T)
 {
 private:
-	Address 					_address;		// socket's address
+	Address	_address;	// socket's address
 	
-	ReadHandler				_recvHandler = null;		// recv's delegate
-	WriteHandler				_sendHandler = null;		// send's delegate
-	CloseHandler				_callBack = null;	// just call back with (void *) when you needed
+	ReadHandler	_recvHandler = null;	// recv's delegate
+	WriteHandler	_sendHandler = null;	// send's delegate
+	CloseHandler	_callBack = null;	// just call back with (void *) when you needed
 	
-	ubyte[] 			_recvBuffer;	// recvBuffer for recv
+	ubyte[]	_recvBuffer;	// recvBuffer for recv
 	
-	SqQueue!(WriteBuffer,true)		_sendQueue;	// send's vector
+	SqQueue!(WriteBuffer,true)	_sendQueue;	// send's vector
 	
 	SOCKET_STATUS	_status = SOCKET_STATUS.IDLE;	// socket's status
-	StatusCallBck				_statusHandler = null;
+	StatusCallBck	_statusHandler = null;
 
 package :
 	/** 设置当前socket的状态 */
-	@property status (SOCKET_STATUS sStatus)
-	{
+	@property status(SOCKET_STATUS sStatus) {
 		auto s = _status;
 		_status = sStatus;
 		if(_statusHandler)
 			_statusHandler(s,_status);
 	}
 	
-	@property void remoteAddress(Address adr){_address = adr;}
+	@property void remoteAddress(Address adr) { _address = adr; }
 
-	bool      _start = false;
-
+	bool _start = false;
 	T listener;
 public:
 
-	bool isInListener(){return (listener !is null);}
+	bool isInListener() { return (listener !is null); }
 
-	void clearListenr(){
+	void clearListenr() {
 		if(isInListener()) {
 			listener.linkMap[fd] = null;
 			listener.linkMap.remove(fd);
@@ -221,102 +208,89 @@ public:
 
 	/** 链接到服务器 */
 	
-	bool connect(Address adr)
-	{
-		if(!_statusHandler) return false;
+	bool connect(Address adr) {
+		if(!_statusHandler)
+			return false;
 		_address = adr;
 		onClose();
 		_status = SOCKET_STATUS.IDLE;
-		if (_address.isIpV6) {
+		if(_address.isIpV6) {
 			fd = socket(AF_INET6, SOCK_STREAM, IPPROTO_IP);
 		} else {
 			fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
 		}
-		if (isInValid())
-		{
+		if(isInValid()) {
 			error("create socket error");
 			return false;
 		}
 		start();
 		int ru = .connect(fd,_address.sockAddr,_address.sockAddrLen);
 		status = SOCKET_STATUS.CONNECTING;
-		if (ru < 0 && errno != EINPROGRESS)
+		if(ru < 0 && errno != EINPROGRESS)
 			return false;
 		return true;
 	}
 
 	/** 设置有数据时的回调 */
-	void readHandler (const ReadHandler  handler)
-	{
+	void readHandler(const ReadHandler handler) {
 		assert (handler);
-		
 		_recvHandler = handler;
 	}
-	
+
 	/** 设置数据写入成功时的回调 */
-	void writeHandler (const WriteHandler handler)
-	{
+	void writeHandler(const WriteHandler handler) {
 		assert (handler);
-		
 		_sendHandler = handler;
 	}
-	
+
 	/** 设置数据关闭时的回调 */
-	void colsedHandler (const CloseHandler callBack)
-	{
+	void colsedHandler(const CloseHandler callBack) {
 		assert (callBack);
-		
 		_callBack = callBack;
 	}
 
-
-	void statusHandler(const StatusCallBck con)
-	{
+	void statusHandler(const StatusCallBck con) {
 		assert(con);
 		_statusHandler = con;
 	}
 
-	
 	/** 发送数据。
 	 @param : data = 要所发送的数据。
 	 @note : 数据会被内部保存和使用，请传过来后不要再对次数据进行操作。如果改变数据，则发送的的数据可能是不确定的。
 	 */
-	bool write(ubyte[] data)
-	{
+	bool write(ubyte[] data) {
 		//info("tcp write data = ", data);
 		trace("write data!");
 
-		if (data.length == 0 || isInValid || status != SOCKET_STATUS.CONNECTED)
-		{
+		if(data.length == 0 || isInValid || status != SOCKET_STATUS.CONNECTED) {
 			error("data's length is 0 when write");
-			
 			return false;
 		}
-		if(!_sendQueue.enQueue(WriteBuffer(data))) return false;
+		if(!_sendQueue.enQueue(WriteBuffer(data)))
+			return false;
 		doWrite();
 
 		return true;
 	}
 	
 	/** 禁用默认构造函数 */
-	@disable this ();
+	@disable this();
 	
 	
 	/** 返回当前socket的状态 */
-	@property status ()
-	{
+	@property status() {
 		return _status;
 	}
 	
-	@property Address remoteAddress(){return _address;}
+	@property Address remoteAddress() { return _address; }
 	
 	/** 把socket加入到事件队列中，开始监听其事件。
 	 @param : 要加到的事件队列。如果为null则为构造时的事件队列。
 	 @return ： 如果已经在事件队列中或者socket无效，则返回false，成功返回true。
 	 */
 	bool start(EventLoop loop = null) {
-		if (!_start && !isInValid ()) {
-			if (loop !is null) {
+		if(!_start && !isInValid ()) {
+			if(loop !is null) {
 				eventLoop = loop;
 			}
 			eventLoop.addEvent(this);
@@ -325,18 +299,16 @@ public:
 		}
 		return false;
 	}
-
 }
 
 
-unittest
-{
+unittest {
 	import std.stdio;
 	EventLoop  loop = new EventLoop();
 	TCPSocket soc = new TCPSocket(loop);
 	Address addr = Address("127.0.0.1",8194);
-	void connectH(bool istrue)
-	{
+
+	void connectH(bool istrue) {
 		writeln("Connect CallBack : ",to!string(istrue));
 	}
 	soc.connectHandler(&connectH);
