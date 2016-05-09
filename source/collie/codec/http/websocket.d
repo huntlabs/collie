07e662f12c3e5bad35a028f5510986c58f099348
渡世白玉 <dushibaiyu@yahoo.com>
2016-05-09 17:23:15 +0800

    fix import name to lower,

--- source/collie/codec/http/websocket.d
+++ source/collie/codec/http/websocket.d
@@ -6,7 +6,6 @@ import std.bitmanip;
 import std.experimental.logger;
 
 import collie.buffer.buffer;
-import collie.buffer.SectionBuffer;
 import collie.codec.http.handler;
 
 enum WebSocketGuid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
