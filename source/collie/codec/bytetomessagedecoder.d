ad8c53c83b7f8dfd6e9590baee95d55ffca38a9b
渡世白玉 <dushibaiyu@yahoo.com>
2016-05-10 18:11:09 +0800

    add mesage to ubyte[]

---
+++ source/collie/codec/bytetomessagedecoder.d
@@ -0,0 +1,19 @@
+module collie.codec.bytetomessagedecoder;
+
+import collie.channel;
+
+abstract class ByteToMessageDecoder(M) : InboundHandler!(ubyte[],M)
+{
+     
+    bool decode(Context* ctx, ubyte[] buf, ref M result);
+
+    override void read(Context* ctx, ubyte[] msg)  
+    {
+        bool success = true;
+        M result;
+        success = decode(ctx, msg, result);
+        if (success) {
+            ctx.fireRead(result);
+        }
+    }
+}
---
+++ source/collie/codec/messagetobyteencoder.d
@@ -0,0 +1,33 @@
+module collie.codec.messagetobyteencoder;
+
+import collie.channel.handler;
+import collie.channel;
+
+abstract class MessageToByteEncoder(M) : OutboundHandler!(M,byte[])
+{
+    ubyte[] encode(ref M msg);
+    
+    void callBack(ubyte[] data,uint len);
+
+    override void write(Context ctx, Win msg, OutboundHandlerCallBack cback = null) 
+    {
+        auto buf = encode(msg);
+        if(buf.ptr)
+        {       
+            ctx.fireWrite(buf,&callBack);
+            if(cback)
+            {
+                import std.traits;
+                static if(isArray!M)
+                    cback(msg,M.length);
+                else
+                    cback(msg,M.sizeof);
+            }
+        }
+        else
+        {
+            if(cback)cback(msg,0); 
+        }
+        
+    }
+}
