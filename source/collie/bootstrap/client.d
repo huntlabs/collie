d8892ffdf213780fac16f57129d846f63a3c661c
donglei@donglei.com <donglei@donglei.com>
2016-05-20 11:19:32 +0800

    Batch add document notes

--- source/collie/bootstrap/client.d
+++ source/collie/bootstrap/client.d
@@ -1,3 +1,13 @@
+/*
+ * Collie - An asynchronous event-driven network framework using Dlang development
+ *
+ * Copyright (C) 2015-2016  Shanghai Putao Technology Co., Ltd 
+ *
+ * Developer: putao's Dlang team
+ *
+ * Licensed under the Apache-2.0 License.
+ *
+ */
 module collie.bootstrap.client;
 
 import std.socket;
@@ -115,4 +125,3 @@ class NeedPipeFactoryException : Exception
         super(msg,file,line);
     }
 }
-
--- source/collie/utils/queue.d
+++ source/collie/utils/queue.d
@@ -1,6 +1,13 @@
-/* Copyright collied.org 
+/*
+ * Collie - An asynchronous event-driven network framework using Dlang development
+ *
+ * Copyright (C) 2015-2016  Shanghai Putao Technology Co., Ltd 
+ *
+ * Developer: putao's Dlang team
+ *
+ * Licensed under the Apache-2.0 License.
+ *
  */
-
 module collie.utils.queue;
 /** 自定义自己的queue的类,非线程安全
  *  @authors : Putao‘s Collie Team
