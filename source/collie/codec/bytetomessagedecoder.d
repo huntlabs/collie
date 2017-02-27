192408c7a47c191c1b0247633543c5e089decd81
dushibaiyu <dushibaiyu@yahoo.com>
2017-02-27 12:22:59 +0800

    fix websocket erro

--- source/collie/codec/bytetomessagedecoder.d
+++ source/collie/codec/bytetomessagedecoder.d
@@ -17,7 +17,7 @@ abstract class ByteToMessageDecoder(M) : InboundHandler!(ubyte[], M)
 
     bool decode(Context ctx, ubyte[] buf, ref M result);
 
-	override void read(Context ctx, ubyte[] msg)
+    override void read(Context ctx, ubyte[] msg)
     {
         bool success = true;
         M result;
